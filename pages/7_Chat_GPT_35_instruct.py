98df88f7cd2f93d1d9658c19d6baeee6653a4d4f
moujxiaXia <18929392955@189.cn>
2023-09-23 21:29:57 +0800

    Update 7_Chat_GPT_35_instruct.py

--- pages/7_Chat_GPT_35_instruct.py
+++ pages/7_Chat_GPT_35_instruct.py
@@ -23,7 +23,7 @@ if prompt := st.chat_input():
     openai.api_key = openai_api_key
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = openai.Completion.create(model="gpt-3.5-turbo-instruct", prompt=st.session_state.messages)
-    msg = response.choices[0].text
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo-instruct", messages=st.session_state.messages)
+    msg = response.choices[0].message
     st.session_state.messages.append(msg)
     st.chat_message("assistant").write(msg.content)
